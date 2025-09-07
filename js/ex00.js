47d8d7c099e8564f11e76cec02ffc297639f521b
Helena Hagsma <helenahagsma2024@gmail.com>
2025-09-07 14:05:34 -0300

    Atualização

--- js/ex00.js
+++ js/ex00.js
@@ -1,26 +1,30 @@
-const numeroSecreto = Math.floor(Math.random() * 100) + 1; /*Gera um numero aleatorio */ /*essa linha cria e guarda um número aleatório entre 1 e 100 na variável numeroSecreto*/
+// Gera um número aleatório entre 1 e 100
+const numeroSecreto = Math.floor(Math.random() * 100) + 1;
 
-const input = document.getElementById("guessInput");
-const button = document.getElementById("guessButton");
-const message = document.getElementById("message");
+function analisarAdivinhacao() {
+  const input = document.getElementById("adivinharInput");
+  const feedback = document.getElementById("feedback");
+  const palpite = Number(input.value);
 
-/*getElementById busca no HTML um elemento que tenha o id indicado. */
-  
-  if (palpite < 1 || palpite > 100 || !palpite) {
-    alert("Digite um número entre 1 e 100!")
+  if (palpite < 1 || palpite > 100) {
+    alert ("Por favor, digite um número entre 1 e 100.")
     return;
+  }
+
+  if (palpite < numeroSecreto) {
+    feedback.textContent = "Muito baixo!👀";
+    feedback.style.color = "darkblue";
+
+  } else if (palpite > numeroSecreto) {
+    feedback.textContent = "Muito alto!👀";
+    feedback.style.color = "darkred";
+   
+  } else {
+    feedback.textContent = "🎉 Você acertou!";
+    feedback.style.color = "green";
+    
+  }
+
+  input.value = ""; // Limpa o campo para nova tentativa
+  input.focus(); // Foca novamente no campo de input
 }
-
-  if (palpite< numeroSecreto){
-    alert("Muito baixo!")
-  }
-  else if(palpite>numeroSecreto){
-    alert("Muito alto!")
-  }
-  else{
-    alert("Voce acertou!")
-    button.disabled = true;
-    input.disabled = true;
-  }
-
-};
