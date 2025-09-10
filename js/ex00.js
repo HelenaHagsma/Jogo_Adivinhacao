5a1cfc98b0ed8d6c8003e7b6bbb62bbeae094df8
Helena Hagsma <helenahagsma2024@gmail.com>
2025-09-10 14:39:22 -0300

    Update ex00.js

--- js/ex00.js
+++ js/ex00.js
@@ -1,10 +1,10 @@
 // Gera um número aleatório entre 1 e 100
 const numeroSecreto = Math.floor(Math.random() * 100) + 1;
 
-function analisarAdivinhacao() {
-  const input = document.getElementById("adivinharInput");
-  const feedback = document.getElementById("feedback");
-  const palpite = Number(input.value);
+function analisarAdivinhacao() { // Função chamada quando o jogador clica no botão de verificar o palpite
+  const input = document.getElementById("adivinharInput"); // Obtém o campo de input onde o usuário digita o palpite
+  const feedback = document.getElementById("feedback"); // Obtém o elemento onde o feedback será exibido (mensagens de acerto ou erro)
+  const palpite = Number(input.value); // Converte o valor digitado pelo usuário (string) para número
 
   if (palpite < 1 || palpite > 100) {
     alert ("Por favor, digite um número entre 1 e 100.")
